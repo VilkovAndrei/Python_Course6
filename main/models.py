945f9bd8e050108c42a1ede131bbe5d6ac20a158
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-05-18 21:42:41 +0300

    add main: models.py Client

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
+from datetime import datetime
+
+from django.conf import settings
+
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    email = models.EmailField(max_length=150, unique=True, verbose_name='Почта')
+    name = models.CharField(max_length=100, verbose_name='ФИО или Наименование')
+    comments = models.TextField(verbose_name='Комментарий', **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='Пользователь', null=True)
+
+    def __repr__(self):
+        return f'{self.name} {self.email}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+        ordering = ('id',)
